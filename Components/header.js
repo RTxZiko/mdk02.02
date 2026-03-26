22d13ef4bd01cad0e2c18995bd073fec8c7db691
koobn <141915048+RTxZiko@users.noreply.github.com>
2026-03-26 14:42:46 +0900

    Add files via upload

---
+++ Components/header.js
@@ -0,0 +1,16 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header class="header">
+                <div class="logo-img">
+                    <img src="https://img.icons8.com/?size=100&id=tdYiJO7G7zvI&format=png&color=000000">
+                </div>
+                <div class="title">
+                    <p>FitPlan</p>
+                </div>
+                
+            </header>
+        `;
+    }
+}
+customElements.define('my-header', MyHeader);
